a3857c12590ad2ad3a6355399202fb4892fab052
Andres Henderson <ahenderson.holberton@gmail.com>
2021-12-08 04:36:39 -0800

    changed exit

--- hsh_shell.c
+++ hsh_shell.c
@@ -32,7 +32,7 @@ int main(int ac, char **av, char **env)
 					if (status == 200)
 					{
 						free(line);
-						return (0);
+						exit(2);
 					}
 				if (is_path == 0)
 					free(commands[0]);
